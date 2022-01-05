b380852de8b55604d768738a55d53c874b2e54b5
Aleksandr <aatrokin@yandex.ru>
2022-01-06 01:13:17 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if ( n > 0){
+  const str = n + '';
+  const xr = str.split('').reverse().join('');
+  resu = xr;
+  } else {
+    str = (n * -1) + '';
+    xr = str.split('').reverse().join('');
+    resu = xr;
+  }
+  return resu;
 }
